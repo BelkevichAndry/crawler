8d5aae825b5b16b554f9b9db20a9ceb821f7fb71
BelkevichAndrew <belkevichandry@gmail.com>
2018-07-20 16:50:00 +0300

    small changes

--- crawler.js
+++ crawler.js
@@ -3,7 +3,6 @@ import client from './services/reddis'
 
 const allVacancies = 'https://api.hh.ru/vacancies?text=java&per_page=100&page=1';
 
-
 async function get(page) {
     const requestURL = `${'https://api.hh.ru/vacancies?text=java&per_page=100&page=' + page }`;
     let parsedData;
@@ -15,7 +14,7 @@ async function get(page) {
             let result = await makeRequest(item.url);
             const parsedRes = JSON.parse(result);
             parsedRes.key_skills.forEach(skill => {
-                    frequency.push(skill);
+                frequency.push(skill);
             })
         }
         return frequency;
@@ -26,16 +25,15 @@ async function get(page) {
 }
 
 const init = async () => {
-
     const res = await makeRequest(allVacancies);
     const parserRes = JSON.parse(res);
     const result = [];
     const frequency = {};
     const pages = parserRes.pages;
-    for(let i = 1;i <2;i++){
+    for (let i = 1; i <pages; i++) {
         console.log(i);
-         let data = await get(i);
-         result.push(data);
+        let data = await get(i);
+        result.push(data);
     }
     const flat = result.flatten(2);
     flat.forEach(skill => {
@@ -46,25 +44,9 @@ const init = async () => {
             frequency[skill.name] = 1;
         }
     });
-    const sorted = sortAssocObject(frequency);
+    client.hmset("java", frequency);
+    client.quit();
 
 };
 
-function sortAssocObject(list) {
-    var sortable = [];
-    for (var key in list) {
-        sortable.push([key, list[key]]);
-    }
-
-    sortable.sort(function(a, b) {
-        return (a[1] > b[1] ? -1 : (a[1] < b[1] ? 1 : 0));
-    });
-
-    var orderedList = {};
-    for (var i = 0; i < sortable.length; i++) {
-        orderedList[sortable[i][0]] = sortable[i][1];
-    }
-
-    return orderedList;
-}
 init();
