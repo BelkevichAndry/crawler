687e20b13c9b4462083a77efa76006318247c373
andrei.zaharenko <frostol@yandex.ru>
2020-02-11 19:52:13 +0300

    added ui elements

--- web/src/components/app.js
+++ web/src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button } from 'react-bootstrap';
 
 import '../styles/App.css';
 
@@ -7,6 +8,15 @@ class App extends Component {
         return (
             <div>
                 <h1>My React App!</h1>
+                    <Button variant="primary">Primary</Button>
+                    <Button variant="secondary">Secondary</Button>
+                    <Button variant="success">Success</Button>
+                    <Button variant="warning">Warning</Button>
+                    <Button variant="danger">Danger</Button>
+                    <Button variant="info">Info</Button>
+                    <Button variant="light">Light</Button>
+                    <Button variant="dark">Dark</Button>
+                    <Button variant="link">Link</Button>
             </div>
         );
     }
