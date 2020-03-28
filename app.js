0f0e65770d6c998e44e70d9c504ac4cdebfdc65f
Andrei Zaharenko <frostol@yandex.ru>
2020-03-28 03:35:47 +0300

    added routes and service

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import run from './crawler'
+import router from './routes/routes'
 import {mongoConnector, close, connectToDb, getAllData} from "./services/storage/db-service";
 import express from "express";
 
@@ -9,28 +10,7 @@ const app = express();
 //     .then(run)
 //     .then(close);
 
-app.get('/get-data', async function (req, res) {
-    await mongoConnector()
-    .then(connectToDb)
-    .then(getAllData)
-    .then(close)
-    .then((data) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.send(data)
-    })
-})
-
-app.get('/parse-new-data', function (req, res) {
-    // mongoConnector()
-    //     .then(connectToDb)
-    //     .then(run)
-    //     .then(close);
-    res.send('Parse new Data')
-})
-
-
-
+app.use(router)
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
