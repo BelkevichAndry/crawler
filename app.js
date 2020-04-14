72170c9e1cde58ba482430c5855159a0857a95da
belkevichandry@gmail.com <belkevichandry@gmail.com>
2020-04-14 11:57:00 +0300

    clean from non crawling logic

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import run from './crawler'
-import router from './routes/routes'
-import {mongoConnector, close, connectToDb, getAllData} from "./services/storage/db-service";
-import express from "express";
+import {mongoConnector, close, connectToDb} from "./services/storage/db-service";
 
-const app = express();
 
-// mongoConnector()
-//     .then(connectToDb)
-//     .then(run)
-//     .then(close);
+mongoConnector()
+    .then(connectToDb)
+    .then(run)
+    .then(close);
 
-app.use(router)
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
